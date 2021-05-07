ed496bc9d2230efd2547b3b6db8715fca5f98864
Jay Paek <46653581+jPaek1002@users.noreply.github.com>
2021-05-07 10:30:47 -0700

    stuff

--- src/destiny/core/Event.java
+++ src/destiny/core/Event.java
@@ -1,9 +1,21 @@
 package destiny.core;
 
+/**
+ * 
+ * This class is for all events
+ * 
+ * @author Nathaniel
+ * @version 12/5/2020
+ */
 class Event {
 	
 	private final int mouseX, mouseY;
 	
+	/**
+	 * create Event bawsedon mouse x and y
+	 * @param mX
+	 * @param mY
+	 */
 	public Event(int mX, int mY) {
 		
 		this.mouseX = mX;
